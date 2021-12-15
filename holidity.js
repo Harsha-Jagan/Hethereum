b175a62942e9708a5afe47c0de88c1d9b89d8f95
Harshavardhan <harsha31.jagan@utexas.edu>
2021-12-15 15:51:16 -0600

    added support for sub,mul,div

--- holidity.js
+++ holidity.js
@@ -1,6 +1,16 @@
 const STOP = "STOP";
 const ADD = "ADD";
+const SUB = "SUB";
+const MUL = "MUL";
+const DIV = "DIV";
 const PUSH = "PUSH";
+
+// comparisons
+const LT = "LT";
+const GT = "GT";
+const EQ = "EQ";
+const AND = "AND";
+const OR = "OR";
 
 class Interpreter {
   constructor() {
@@ -12,6 +22,7 @@ class Interpreter {
   }
 
   runCode(code) {
+    this.state.programCounter = 0;
     this.state.code = code;
 
     while (this.state.programCounter < this.state.code.length) {
@@ -27,10 +38,20 @@ class Interpreter {
             this.state.stack.push(value);
             break;
           case ADD:
+          case SUB:
+          case MUL:
+          case DIV:
             const a = this.state.stack.pop();
             const b = this.state.stack.pop();
 
-            this.state.stack.push(a + b);
+            let result;
+            console.log(opCode);
+            if (opCode === ADD) result = a + b;
+            else if (opCode === SUB) result = a - b;
+            else if (opCode === MUL) result = a * b;
+            else if (opCode === DIV) result = a / b;
+
+            this.state.stack.push(result);
             break;
           default:
             break;
@@ -46,8 +67,22 @@ class Interpreter {
 
 function main() {
   const interpreter = new Interpreter();
-  const code = [PUSH, 2, PUSH, 3, ADD, STOP];
-  console.log(interpreter.runCode(code));
+  const code1 = [PUSH, 2, PUSH, 3, ADD, STOP];
+  const code2 = [PUSH, 2, PUSH, 3, SUB, STOP];
+  const code3 = [PUSH, 2, PUSH, 3, MUL, STOP];
+  const code4 = [PUSH, 2, PUSH, 3, DIV, STOP];
+
+  result = interpreter.runCode(code1);
+  console.log(result);
+
+  result = interpreter.runCode(code2);
+  console.log(result);
+
+  result = interpreter.runCode(code3);
+  console.log(result);
+
+  result = interpreter.runCode(code4);
+  console.log(result);
 }
 
 main();
